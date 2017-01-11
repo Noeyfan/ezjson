719e487899fc58e356293a7ec4d1adcc51eaea09
fanyou <youfan.noey@gmail.com>
2017-01-10 23:43:29 -0800

    some naive code to build up the basic TDD framework

--- src/ezjson.cc
+++ src/ezjson.cc
@@ -1,1 +1,75 @@
 #include "ezjson.h"
+#include <iostream>
+#include <cassert>
+
+void parse_json_whitespace(const char* json) {
+    // just consume ws
+    while(json && *json == ' ') {
+	json++;
+    }
+}
+
+parse_status parse_json_value_null(json_value* v, const char* json) {
+    parse_status result = ERROR;
+    if (*json == 'n' &&
+	*(json+1) == 'u' &&
+	*(json+2) == 'l' &&
+	*(json+3) == 'l') {
+	result = OK;
+	v->type = JSON_NULL;
+    }
+
+    return result;
+}
+
+parse_status parse_json_value_boolean(json_value* v, const char* json) {
+    parse_status result = ERROR;
+    if (
+	*json == 'f' &&
+	*(json+1) == 'a' &&
+	*(json+2) == 'l' &&
+	*(json+3) == 's' &&
+	*(json+4) == 'e' ) {
+	result = OK;
+	v->value = false;
+	v->type = JSON_BOOLEAN;
+    }
+
+    if (*json == 't' &&
+	*(json+1) == 'r' &&
+	*(json+2) == 'u' &&
+	*(json+3) == 'e' ) {
+	result = OK;
+	v->value = true;
+	v->type = JSON_BOOLEAN;
+    }
+
+    return result;
+}
+
+int i = 0;
+
+parse_status parse_json_value(json_value* v, const char* json) {
+    assert(json);
+    switch(*json) {
+      case 'f':
+      case 't':
+	return parse_json_value_boolean(v, json);
+      case 'n':
+	return parse_json_value_null(v, json);
+      default:
+	return ERROR;
+    }
+}
+
+json_type get_json_type(const json_value* v) {
+    return v->type;
+}
+
+int parse_json(json_value* v, const char* json) {
+    json_contex jc(json);
+    parse_json_whitespace(jc.context);
+    parse_status result = parse_json_value(v, jc.context);
+    parse_json_whitespace(jc.context);
+    return result;
+}
--- src/ezjson.h
+++ src/ezjson.h
@@ -1,2 +1,30 @@
 #ifndef __EZJSON_H__
+#define __EZJSON_H__
+enum json_type {
+    JSON_NULL,
+    JSON_BOOLEAN,
+    JSON_NUMBER,
+    JSON_STRING,
+    JSON_ARRAY,
+    JSON_OBJECT
+};
+
+enum parse_status {
+    OK,
+    ERROR
+};
+
+struct json_value {
+    json_type type;
+    bool value;
+};
+
+struct json_contex {
+    json_contex(const char* rhs) : context(rhs) { }
+    const char* context;
+};
+
+int parse_json(json_value* v, const char* json);
+json_type get_json_type(const json_value* v);
+
 #endif
